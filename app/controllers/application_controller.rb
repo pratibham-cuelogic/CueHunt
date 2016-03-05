c877416377d205d6dc6eb1c24b1eb62bd0b90e81
Jitendra S <jitendra.samant@cuelogic.co.in>
2016-03-05 17:50:23 +0530

    resend invitation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   skip_before_filter :verify_authenticity_token
-  before_action :authenticate_user!, :check_website_access
+  before_action :authenticate_user!
+  before_action :check_website_access, if: proc { user_signed_in?}
 
 
   def check_website_access
