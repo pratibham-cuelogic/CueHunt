f5fa1add6e639f65e042c5f8fbf2657bead70ab4
Jitendra S <jitendra.samant@cuelogic.co.in>
2016-03-05 17:45:45 +0530

    resend invitation

---
+++ config/initializers/constants.rb
@@ -0,0 +1,10 @@
+#User status
+INACTIVE = 0
+ACTIVE = 1
+INVITED = 2
+INVITATION_ACCEPTED = 2
+NO_ACCESS = -1
+
+# Admin Role
+ADMIN = 'admin'
+USER = 'user'
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     get '/dashboard', to: 'admin/dashboards#index', as: 'admin_dashboard'
     get '/invite-candidate', to: 'admin/users#invite_candidate', as: 'invite_candidate'
     post '/send_invitation_to_candidate', to: 'admin/users#send_invitation_to_candidate', as: 'send_invitation_to_candidate'
+    get '/online-candidates', to: 'admin/users#online_candidates',as: 'online_candidates'
   end
   get '/dashboard', to: 'user_dashboards#dashboard', as: 'user_dashboard'
 
