1476adc1385bffe13f5a2bb430884fc4d42361cf
Jitendra S <jitendra.samant@cuelogic.co.in>
2016-03-05 23:18:25 +0530

    question popup

--- db/migrate/20160305081856_create_question_answers.rb
+++ db/migrate/20160305081856_create_question_answers.rb
@@ -1,5 +1,16 @@
 class CreateQuestionAnswers < ActiveRecord::Migration
   def change
+    create_table :questions do |t|
+      t.text :title
+      t.references :technology, index: true
+
+      t.timestamps
+    end
+    create_table :answers do |t|
+      t.text :description
+
+      t.timestamps
+    end
     create_table :question_answers do |t|
       t.belongs_to :question, index: true
       t.belongs_to :answer, index: true
--- db/migrate/20160305092704_create_question_sets.rb
+++ db/migrate/20160305092704_create_question_sets.rb
@@ -3,7 +3,7 @@ class CreateQuestionSets < ActiveRecord::Migration
     create_table :question_sets do |t|
       t.references :user_set, index: true
       t.references :question, index: true
-      t.integer :answer, index: true
+      t.references :answer, index: true
       t.boolean :is_correct
 
       t.timestamps
