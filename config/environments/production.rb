c9996d80f9542da99533581a1b854a0cdd45cad0
Jitendra S <jitendra.samant@cuelogic.co.in>
2016-03-06 08:55:33 +0530

    new desing for thank you page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
-  config.action_mailer.default_url_options = { host: ' https://sheltered-refuge-33433.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://shielded-badlands-18708.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
